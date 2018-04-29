99458cacd4ed1a151e89e4edeba2905f4b999504
m0mu <honey14111995@yahoo.com>
2018-04-29 16:38:48 +0530

    Project build fixed

---
+++ PermissionAnalysis/app/src/main/java/com/example/momo/permission_analysis/AppsAdapter_2_Backup.java
@@ -0,0 +1,171 @@
+package com.example.momo.permission_analysis;//package com.android_examples.getinstalledappiconname_android_examplescom;
+//
+//
+//
+//import android.Manifest;
+//import android.app.ActivityManager;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.pm.PackageInfo;
+//import android.content.pm.PackageManager;
+//import android.content.res.Resources;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//import android.net.Uri;
+//import android.os.Handler;
+//import android.speech.tts.TextToSpeech;
+//import android.support.v4.content.ContextCompat;
+//import android.support.v4.content.PermissionChecker;
+//import android.util.Log;
+//import android.widget.CheckedTextView;
+//import android.widget.ImageView;
+//import android.support.v7.widget.RecyclerView;
+//import android.widget.Toast;
+//import android.view.View;
+//import android.widget.TextView;
+//import android.graphics.drawable.Drawable;
+//import android.support.v7.widget.CardView;
+//import android.view.LayoutInflater;
+//import android.view.ViewGroup;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.FileOutputStream;
+//import java.util.List;
+//
+//import static android.Manifest.permission.RECORD_AUDIO;
+//import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+//import static android.support.v4.content.PermissionChecker.checkSelfPermission;
+//
+//public class AppsAdapter_2 extends RecyclerView.Adapter<AppsAdapter_2.ViewHolder>{
+//
+//    Context context1;
+//    List<String> stringList;
+//
+//
+//
+//
+////    public android.support.v7.widget.RecyclerView.Adapter send_Context_and_List(Context context, List<String> list)
+////    {
+////        context1 = context;
+////        stringList = list;
+////
+////
+////    }
+//
+//    public AppsAdapter_2(Context context, List<String> list){
+//
+//        context1 = context;
+//
+//        stringList = list;
+//    }
+//
+//
+//    public static class ViewHolder extends RecyclerView.ViewHolder{
+//
+//        public CardView cardView;
+//        public TextView textView_App_Package_Name;
+//        public TextView textView_perm_Name;
+//        public CheckedTextView chk;
+//
+//
+//
+//
+//        public ViewHolder (View view){
+//
+//            super(view);
+//
+//            cardView = (CardView) view.findViewById(R.id.card_view_2);
+//            textView_App_Package_Name = (TextView) view.findViewById(R.id.Apk_Package_Name);
+//            textView_perm_Name = (TextView) view.findViewById(R.id.Permissions);
+//            chk = (CheckedTextView) view.findViewById(R.id.ctv);
+//        }
+//    }
+//
+//    @Override
+//    public AppsAdapter_2.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+//
+//        View view2 = LayoutInflater.from(context1).inflate(R.layout.cardview_2,parent,false);
+//
+//        ViewHolder viewHolder = new ViewHolder(view2);
+//
+//        return viewHolder;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(final ViewHolder viewHolder, int position) {
+//
+//        final String ApplicationPackageName = (String) stringList.get(position);//
+//        viewHolder.textView_App_Package_Name.setText(ApplicationPackageName);
+//        viewHolder.chk.setChecked(true);
+//
+//
+//
+//        //Adding click listener on CardView to open clicked application directly from here .
+//        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                viewHolder.chk.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        if (viewHolder.chk.isChecked()) {
+////                            viewHolder.chk.setText("Permission Denied!");
+//                            final Toast toast = Toast.makeText(context1,ApplicationPackageName+" Permission Denied!", Toast.LENGTH_SHORT);
+//                            toast.show();
+//                            viewHolder.chk.setChecked(false);
+//                            Handler handler = new Handler();
+//                            handler.postDelayed(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    toast.cancel();
+//                                }
+//                            }, 500);
+//
+//                            // ADD CODE TO REMOVE THE PERMISSION FROM THE MANIFEST
+//
+//
+//
+//
+//                        } else {
+////                            viewHolder.chk.setText("Permission Granted!");
+//                            final Toast toast = Toast.makeText(context1,ApplicationPackageName+" Permission Granted!", Toast.LENGTH_SHORT);
+//                            toast.show();
+//                            viewHolder.chk.setChecked(true);
+//                            Handler handler = new Handler();
+//                            handler.postDelayed(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    toast.cancel();
+//                                }
+//                            }, 500);
+//                        }
+//
+//
+//                        // ADD CODE TO ADD THE PERMISSION TO THE MANIFEST
+//
+//
+////                Intent intent = context1.getPackageManager().getLaunchIntentForPackage(ApplicationPackageName);
+////                if(intent != null){
+////                    Intent intent1 = new Intent(context1, Main_Activity2.class);
+////                    //// TEST
+////                    intent1.putExtra("ApplicationPackageName", ApplicationPackageName);
+////                    //// TEST
+////                    context1.startActivity(intent1);
+////                }
+////                else {
+////
+////                    Toast.makeText(context1,ApplicationPackageName + " Error, Please Try Again.", Toast.LENGTH_LONG).show();
+////                }
+//                    }
+//                });
+//            }
+//        });
+//    };
+//
+//    @Override
+//    public int getItemCount(){
+//
+//        return stringList.size();
+//    }
+//
+//}
---
+++ PermissionAnalysis/app/src/main/java/com/example/momo/permission_analysis/Main_Activity2.java
@@ -0,0 +1,141 @@
+package com.example.momo.permission_analysis;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+
+public class Main_Activity2 extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager recyclerViewLayoutManager;
+    List<String> all_permissions = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+
+
+        String applicationPackageName = getIntent().getStringExtra("ApplicationPackageName");
+        ApkInfoExtractor apk = new ApkInfoExtractor(Main_Activity2.this);
+        ImageView imgView = (ImageView) findViewById(R.id.imageView);
+        imgView.setImageDrawable(apk.getAppIconByPackageName(applicationPackageName));
+
+//        TextView textView = (TextView) findViewById(R.id.Permission);
+//        textView.setText(getIntent().getStringExtra("Permission"));
+        final String perm = apk.GetPermissions(Manifest.permission.CAMERA, applicationPackageName);
+//        textView.setText(perm);
+
+
+//        final CheckedTextView ctv = (CheckedTextView) findViewById(R.id.checkedTextView1);
+//        if (perm.equalsIgnoreCase( "Permission Granted!"))
+//        {
+//            ctv.setText("Permission Granted!");
+//            ctv.setChecked(true);
+//        }
+////                if (ctv.isChecked())
+////                    ctv.setChecked(false);
+//        else
+//            ctv.setText("Permission Denied!");
+//            ctv.setChecked(false);
+
+        all_permissions = apk.Get_Granted_Permissions(applicationPackageName);
+
+        int count = 0;
+        for (String permission: all_permissions)
+        {
+            all_permissions.set(count, permission.replace("android.permission.",""));
+            count ++;
+
+        }
+
+        final CheckedTextView ctv = (CheckedTextView) findViewById(R.id.checkedTextView1);
+        if (perm.equalsIgnoreCase( "Permission Granted!"))
+        {
+            ctv.setText(perm);
+            ctv.setChecked(true);
+
+        }
+//                if (ctv.isChecked())
+//                    ctv.setChecked(false);
+        else {
+            ctv.setText(perm);
+        }
+
+        ctv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ctv.isChecked()) {
+                    ctv.setText("Permission Denied!");
+                    ctv.setChecked(false);
+                }
+                else
+                {
+                    ctv.setText("Permission Granted!");
+                    ctv.setChecked(true);
+                }
+
+            }
+        });
+
+//        // Source code doesn't match byte code error in library for app icon
+//        String icon = getIntent().getStringExtra("icon");
+//        ApkInfoExtractor apk = new ApkInfoExtractor(main1.send_context);
+//
+//        ImageView imgView = (ImageView) findViewById(R.id.imageView);
+//        imgView.setImageDrawable(apk.getAppIconByPackageName(icon));
+
+
+        // Function for all granted permissions begin
+
+
+
+
+
+
+        // Function for all granted permissions end
+
+
+
+
+        //Testing begin for Recycler View
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_2);
+
+        // Passing the column number 1 to show on line one column in each row.
+        recyclerViewLayoutManager = new GridLayoutManager(Main_Activity2.this, 1);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+        adapter = new AppsAdapter_2(Main_Activity2.this, all_permissions);
+        recyclerView.setAdapter(adapter);
+
+
+        // Testing end for Recycler View
+
+        }
+
+}
+
